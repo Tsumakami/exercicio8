4797cd97f721ac1095fb59573efb7043446e6d05
João Paulo Malvesti <jpmf1995@gmail.com>
2022-07-03 15:22:06 -0300

    Added histogram, mirror and filters

---
+++ js/filters/maximum-filter.js
@@ -0,0 +1,38 @@
+const maximumFilter = {
+  kernelMapping: {
+    '3': 1,
+    '5': 2,
+    '7': 3
+  },
+  apply: function(matrix, width, filterSize) {
+    const lineSeparated = separateMatrixIntoLines(matrix, width);
+    const lineAndChunkSeparated = separateChunkIntoPixels(lineSeparated);
+    const kernelSize = this.kernelMapping[filterSize.toString()];
+  
+    for (let i = 0; i< lineAndChunkSeparated.length; i++) {
+      for (let j = 0; j < lineAndChunkSeparated[i].length; j++) {
+        let maximumValue = [0, 0, 0, 0];
+  
+        for (let l = i - kernelSize; l < i + kernelSize; l++) {
+          for (let m = j - kernelSize; m <= j + kernelSize; m++) {
+            if (lineAndChunkSeparated[l] && lineAndChunkSeparated[l][m]) {
+              lineAndChunkSeparated[l][m].forEach((pixelVal, idx) => {
+                if (maximumValue[idx] < pixelVal) {
+                  maximumValue[idx] = pixelVal;
+                }
+              })
+            }
+          }
+        }
+  
+        lineAndChunkSeparated[i][j][0] = maximumValue[0];
+        lineAndChunkSeparated[i][j][1] = maximumValue[1];
+        //TODO ajustar Blue
+        lineAndChunkSeparated[i][j][2] = maximumValue[1];
+        lineAndChunkSeparated[i][j][3] = 255;
+      }
+    }
+  
+    return flattenV2(lineAndChunkSeparated);  
+  }
+}
--- js/imageHelps.js
+++ js/imageHelps.js
@@ -25,6 +25,27 @@ function imageDataToImage(imagedata) {
     return image;
 }
 
+function matrixToImageData(matrix, width, height) {
+    const typedArray = new Uint8ClampedArray(matrix.length);
+    for (let i = 0; i < matrix.length - 4; i += 4) {
+      typedArray[i] = matrix[i];
+      typedArray[i+ 1] = matrix[i + 1];
+      typedArray[i + 2] = matrix[i + 2];
+      typedArray[i + 3] = 255;
+    }
+
+    const imgData = new ImageData(typedArray, width, height);
+
+    return imgData;
+}
+
+function matrixToImage(matrix, width, height){
+    const imageData = matrixToImageData(matrix, width, height);
+    const image = imageDataToImage(imageData);
+
+    return image;
+}
+
 function truncateValues(value) {
     return value > 255 ? 255 : value;
 }
@@ -44,3 +65,43 @@ function resolveOverflow(value){
 function resolveUnderflow(value) {
     return value < 0 ? 0 : value;
 }
+
+function showResult(image, divId) {
+    let outputResult = document.getElementById(divId);
+    outputResult.src = image.src;
+}
+
+function flatten(flipped) {
+    return flipped.reduce((acc, curr) => acc = [...acc, ...curr], []);
+}
+
+function flattenV2(arr) {
+    const result = [];
+    arr.forEach(line => line.forEach(pixelChunk => pixelChunk.forEach(pixelVal => result.push(pixelVal))));
+
+    return result;
+}
+
+function separateMatrixIntoLines(matrix, width) {
+    const result = [];
+    
+    for (let i = 0; i < matrix.length; i += width * 4) {
+        result.push(matrix.slice(i, i + width * 4));
+    }
+
+    return result;
+}
+
+function separateChunkIntoPixels(matrix) {
+    return matrix.map(line => {
+        const newLine = [];
+        for (let i = 0; i < line.length; i += 4) {
+        const pixelChunk = [line[i], line[i + 1], line[1 + 2], line[i + 3]];
+        newLine.push(pixelChunk);
+        }
+
+        return newLine;
+    });
+}
+
+const transformToDrawChart = (arr) => arr.map((item, idx) => [idx, item]);
